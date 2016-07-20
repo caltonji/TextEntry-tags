af572dc604836fc2ba7657d1a4a42a92ffe9aeab
caltonji <caltonji@gmail.com>
2016-07-20 03:24:45 -0700

    first commit.  UI is working with cookie storage.  A bit unusable but needs some outside feedback and motivation before moving forwards.

--- public/core.js
+++ public/core.js
@@ -1,32 +1,92 @@
-// public/core.js
-var writer = angular.module('writer', []);
+(function() {
 
-function mainController($scope) {
-    $scope.thoughts = [];
+    angular
+        .module("notes", ['ngCookies'])
+        .controller("mainController", ["$scope", "$cookies", mainController]);
 
-    $scope.placeholderThought = "Type a thought and press enter...";
-    $scope.currentThought = "";
-
-    var adjustScroll = function() {
-        $('body').scrollTop($('body')[0].scrollHeight);
-    }
-
-    var addNewThought = function(newThought) {
-        $scope.thoughts.push({message:newThought});
-        $scope.$apply();
-        adjustScroll();
-    }
-
-    $('div[contenteditable]').keydown(function(e) {
-        var that = this;
-        // trap the return key being pressed
-        if (e.keyCode === 13) {
-          e.preventDefault();
-          addNewThought($(that).text());
-          $(that).text("");
-          $(that).focus();
-          // prevent the default behaviour of return key pressed
-          
+    function mainController($scope, $cookies) {
+        /* static data */
+        // $scope.tags = ["Ben Franklin", "Hackathon Ideas", "Art", "Recipes"];
+        // $scope.tagsToNotes = {
+        //     "Ben Franklin" : ["lorem ipsum", "and other stuff"], 
+        //     "Hackathon Ideas" : ["more other stuff", "fuck I hate mock data"],
+        //     "Art" : [],
+        //     "Recipes" : []
+        // };
+        $scope.clearDatabase = function clearDatabase() {
+            $cookies.put('notes-tags', null);
+            $cookies.put('notes-tagsToNotes', null);
         }
-    });
-}
+        // $scope.clearDatabase();
+        var tagsFromStore = JSON.parse($cookies.get('notes-tags'));
+        var tagsToNotesFromStore = JSON.parse($cookies.get('notes-tagsToNotes'));
+        $scope.tags = tagsFromStore || [];
+        $scope.tagsToNotes = tagsToNotesFromStore || {};
+
+        console.log({tags: $scope.tags, tagsToNotes: $scope.tagsToNotes});
+        $scope.textareaPlaceholder = "Type a new idea... or an old one...";
+        $scope.currentTag = $scope.tags[0];
+        $scope.currentNote = "";
+        
+
+        $scope.submitNoteText = "Add a tag to get started"
+        $scope.submitNoteEnabled = false;
+        var enableSubmitNote = function enableSubmitNote() {
+            $scope.submitNoteEnabled = true;
+            $scope.submitNoteText = "Submit Note";
+        }
+        if ($scope.currentTag) {
+            enableSubmitNote();
+        }
+        
+        $scope.newTagPlaceholder = "Add tag";
+        $scope.currentNewTag = "";
+        $scope.newTagFocus = false;
+
+        $scope.focused = function focused() {
+            console.log("ford focus");
+        }
+        $scope.currentTagSize = Math.max($scope.newTagPlaceholder.length, $scope.currentNewTag.length);
+        $scope.currentNewTagInputSize = function currentNewTagInputSize() {
+            return Math.max($scope.newTagPlaceholder.length, $scope.currentNewTag.length);
+        }
+        $scope.submitTag = function submitTag($event) {
+            var newTag = $scope.currentNewTag;
+            if (newTag && newTag.length > 0 && $scope.tags.indexOf(newTag) == -1) {
+                $scope.tags.push(newTag);
+                $scope.tagsToNotes[newTag] = [];
+                $scope.currentTag = newTag;
+                $scope.currentNewTag = "";
+                $event.target.firstChild.blur();
+                enableSubmitNote();
+                $scope.updateDatabase()
+            }
+        }
+
+        /* functions */
+        $scope.setCurrentTag = function setTag(index) {
+            $scope.currentTag = $scope.tags[index];
+        }
+
+        $scope.submitNote = function submitNote() {
+            if ($scope.currentTag && $scope.currentTag.length > 0) {
+                // push current note and clear it
+                $scope.tagsToNotes[$scope.currentTag].push($scope.currentNote);
+                $scope.currentNote = "";
+                var index = $scope.tags.indexOf($scope.currentTag);
+                $scope.tags.splice(index, 1);
+                $scope.tags.unshift($scope.currentTag);
+                $scope.updateDatabase();
+            }
+        }
+
+        $scope.updateDatabase = function updateDatabase() {
+            $cookies.put('notes-tags', JSON.stringify($scope.tags));
+            $cookies.put('notes-tagsToNotes', JSON.stringify($scope.tagsToNotes));
+        }
+
+    }
+
+})();
+
+
